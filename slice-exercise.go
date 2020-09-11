6f446381eaa6bfb5498761a881ab413860465e94
NLee <49750572+nl900@users.noreply.github.com>
2020-09-11 16:27:13 +1000

    Create slice-exercise.go

---
+++ slice-exercise.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	res := make([][]uint8, dx)
+	for x := range res {
+		res[x] = make([]uint8, dy)
+		for y := range res[x] {
+			res[x][y] = uint8(x*y)
+		}
+	}
+	return res
+}
+
+func main() {
+	pic.Show(Pic)
+}
