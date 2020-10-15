543924b264d3774fb7c9e3f8deaac54c6ed97c33
NLee <49750572+nl900@users.noreply.github.com>
2020-10-15 14:31:15 +1100

    Create exercise-errors.go

---
+++ exercise-errors.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %d", float64(e))
+}
+
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := x/2.0
+	for i:=0; i<10; i++ {
+		z0 := z
+		z -= (z*z - x) / (2*z)
+		//fmt.Println(i)
+		if z0== z {
+			break
+		}
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
